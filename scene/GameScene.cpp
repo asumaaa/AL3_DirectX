378dc20a52c55c9e906d0704d1f84e47e3c65277
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-04-14 14:58:46 +0900

    move sprite

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -58,6 +58,19 @@ void GameScene::Draw() {
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
+	#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
 #pragma region 背景スプライト描画
 	// 背景スプライト描画前処理
 	Sprite::PreDraw(commandList);
@@ -73,18 +86,18 @@ void GameScene::Draw() {
 	dxCommon_->ClearDepthBuffer();
 #pragma endregion
 
-#pragma region 3Dオブジェクト描画
-	// 3Dオブジェクト描画前処理
-	Model::PreDraw(commandList);
-
-	/// <summary>
-	/// ここに3Dオブジェクトの描画処理を追加できる
-	/// </summary>
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
-
-	// 3Dオブジェクト描画後処理
-	Model::PostDraw();
-#pragma endregion
+//#pragma region 3Dオブジェクト描画
+//	 3Dオブジェクト描画前処理
+//	Model::PreDraw(commandList);
+//
+//	 <summary>
+//	 ここに3Dオブジェクトの描画処理を追加できる
+//	 </summary>
+//	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+//
+//	 3Dオブジェクト描画後処理
+//	Model::PostDraw();
+//#pragma endregion
 
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
